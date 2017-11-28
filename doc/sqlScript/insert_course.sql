af3b55251585ae716452a4c3d34eb11dee6b3ac4
yadiel <yaodingw@gmail.com>
2017-11-28 13:44:08 +0800

    添加测试数据的脚本文件

---
+++ doc/sqlScript/insert_course.sql
@@ -0,0 +1,3 @@
+insert into user (name ,age,sex ,create_time) values('张三',20,0,'2014-05-27 18:12:59');
+insert into user (name ,age,sex ,create_time) values('李四',30,0,'2014-05-27 18:12:59');
+insert into user (name ,age,sex ,create_time) values('王二麻子',18,1,'2014-05-27 18:12:59');
